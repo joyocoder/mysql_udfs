a844557a645cb4df74cf643e38dc91c72e035f31
Jo Yo <joyocoder@gmail.com>
2010-02-11 23:44:59 -0600

    sql installer

---
+++ install.sql
@@ -0,0 +1,24 @@
+/* 
+*/
+
+use mysql;
+
+drop function dotf;
+drop function doti;
+drop function dist2f;
+drop function dist2i;
+drop function jit;
+drop function tagcloud;
+drop function isum;
+drop function isumn;
+drop function jsmap;
+
+create function dotf                 returns REAL   soname 'libjoyo.so';
+create function doti                 returns REAL   soname 'libjoyo.so';
+create function dist2f               returns REAL   soname 'libjoyo.so';
+create function dist2i               returns REAL   soname 'libjoyo.so';
+create function jit                  returns INT    soname 'libjoyo.so';
+create function jsmap                returns STRING soname 'libjoyo.so';
+create aggregate function tagcloud   returns STRING soname 'libjoyo.so';
+create aggregate function isum       returns STRING soname 'libjoyo.so';
+create aggregate function isumn      returns STRING soname 'libjoyo.so';
